c16376c522564989b655192064c085133d7d7be7
Tursun <kurbanov.tursunchik@gmail.com>
2025-02-27 17:12:47 +0300

    Fix the mistakes of first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tabs = document.querySelectorAll(".tabheader__item"),
+window.addEventListener("DOMContentLoaded", function () {
+  // Tabs
+
+  let tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
 
@@ -8,21 +10,22 @@ window.addEventListener("DOMContentLoaded", () => {
       item.classList.add("hide");
       item.classList.remove("show", "fade");
     });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
   }
-  tabs.forEach((item) => {
-    item.classList.remove("tabheader__item_active");
-  });
 
   function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show");
-    tabsContent[i].classList.remove("hide", "fade");
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
     tabs[i].classList.add("tabheader__item_active");
   }
 
   hideTabContent();
   showTabContent();
 
-  tabsParent.addEventListener("click", (event) => {
+  tabsParent.addEventListener("click", function (event) {
     const target = event.target;
     if (target && target.classList.contains("tabheader__item")) {
       tabs.forEach((item, i) => {
