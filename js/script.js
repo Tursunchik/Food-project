a68c36eeacc1e1259166001ebf5afc404dff8608
Tursun <kurbanov.tursunchik@gmail.com>
2025-03-20 18:07:24 +0300

    fix classes make them more flexible

--- js/script.js
+++ js/script.js
@@ -145,11 +145,12 @@ window.addEventListener("DOMContentLoaded", function () {
   // Используем классы для создание карточек меню
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
+      this.classes = classes;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
@@ -162,8 +163,12 @@ window.addEventListener("DOMContentLoaded", function () {
 
     render() {
       const element = document.createElement("div");
+      if (this.classes === 0) {
+        (this.element = "menu.element"), element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add("className"));
+      }
       element.innerHTML = `
-              <div class="menu__item">
                   <img src=${this.src} alt=${this.alt}>
                   <h3 class="menu__item-subtitle">${this.title}</h3>
                   <div class="menu__item-descr">${this.descr}</div>
@@ -172,7 +177,6 @@ window.addEventListener("DOMContentLoaded", function () {
                       <div class="menu__item-cost">Цена:</div>
                       <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                   </div>
-              </div>
           `;
       this.parent.append(element);
     }
@@ -193,7 +197,8 @@ window.addEventListener("DOMContentLoaded", function () {
     'Меню "Постное"',
     "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
     14,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -202,6 +207,7 @@ window.addEventListener("DOMContentLoaded", function () {
     "Меню “Премиум”",
     "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
     21,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 });
